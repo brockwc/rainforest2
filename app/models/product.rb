7e1b2e756b3241bdf4e2968596c0eba022b98fb9
zap-rowsdower <brock.whitbread@gmail.com>
2014-04-16 16:21:07 -0400

    writing product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-  
+  validates :description, :name, :presence => true
+  validates :price_in_cents, :numericality => {:only_integer => true}  
 
   def formatted_price
     price_in_dollars = price_in_cents/100
